fc107c71f308fe6e6473fe1cd632f77b03c85b5a
lwu9 <lwu9@ncsu.edu>
2018-09-25 13:42:22 -0400

    stepwise3

--- src/analysis/stepwise.py
+++ src/analysis/stepwise.py
@@ -14,12 +14,12 @@ Created on Tue Sep 25 12:21:41 2018
 #print('p0p1 {}'.format(p0p1))
 #for zeta2 in np.linspace(0, 2, zeta_grid_dimension):
 #  zeta = np.array([0.3, -5, zeta2])
-def rollout_stepwise_linear_mab(zeta, env, J=10, mc_rep=500, time_horizon=100):
+def rollout_stepwise_linear_mab(zeta, env, J=10, mc_rep=500, T=100):
   mean_cum_reward = 0.0
   for rep in range(mc_rep):
     env.reset()
     for t in range(T):
-      epsilon = tune_stepwise_linear_mab(zeta, J, t, time_horizon=100)
+      epsilon = tune_stepwise_linear_mab(zeta, J, t, T=100)
       if np.random.rand() < epsilon:
         action = np.random.choice(2)
       else:
@@ -30,10 +30,10 @@ def rollout_stepwise_linear_mab(zeta, env, J=10, mc_rep=500, time_horizon=100):
   return mean_cum_reward
   
 
-def tune_stepwise_linear_mab(zeta, J, t, time_horizon=100):
+def tune_stepwise_linear_mab(zeta, J, t, T=100):
   ## zeta: parameter vector with length J
-  interval = int(time_horizon/J)
-  j = int(t/interval)
-  epsilon = sum(zeta[:j]) + (t - j*interval) * zeta[j]
+  interval = int(T/J)
+  j = int((T-t)/interval)
+  epsilon = sum(zeta[:j]) + ((T-t) - j*interval) * zeta[j]
   return epsilon
   
