67bed5b998f9754dce89a3433b52b2821e62f67f
jesse <jclifton333@gmail.com>
2019-06-12 13:44:54 +0200

    conduct ht

--- src/hypothesis-test/mab_hypothesis_test.py
+++ src/hypothesis-test/mab_hypothesis_test.py
@@ -50,8 +50,8 @@ def normal_mab_sampling_dbn(true_model_params, num_pulls):
   return xbar_draws
 
 
-def mab_hypothesis_test(baseline_policy, proposed_policy, xbar, true_model, estimated_model, num_pulls,
-                        t, T, sampling_dbn, mc_reps=1000):
+def mab_regret_sampling_dbn(baseline_policy, proposed_policy, true_model, estimated_model, num_pulls,
+                            t, T, sampling_dbn, mc_reps=1000):
   """
 
   :param baseline_policy:
@@ -69,8 +69,8 @@ def mab_hypothesis_test(baseline_policy, proposed_policy, xbar, true_model, esti
   proposed_policy_regrets = []
   for rep in range(mc_reps):
     sampled_model = sampling_dbn(true_model, num_pulls)
-    baseline_regret = true_mab_regret(baseline_policy, true_model, sampled_model, num_pulls, t, T)
-    proposed_regret = true_mab_regret(proposed_policy, true_model, sampled_model, num_pulls, t, T)
+    baseline_regret = true_mab_regret(baseline_policy, true_model, sampled_model, estimated_model, num_pulls, t, T)
+    proposed_regret = true_mab_regret(proposed_policy, true_model, sampled_model, estimated_model, num_pulls, t, T)
     baseline_policy_regrets.append(baseline_regret)
     proposed_policy_regrets.append(proposed_regret)
   diffs = np.array(baseline_policy_regrets) - np.array(proposed_policy_regrets)
@@ -79,7 +79,43 @@ def mab_hypothesis_test(baseline_policy, proposed_policy, xbar, true_model, esti
 
 def cutoff_for_ht(alpha, sampling_dbns):
   cutoffs = [np.percentile(sampling_dbn, (1 - alpha)*100) for sampling_dbn in sampling_dbns]
-  return  np.max(cutoffs)
+  return np.max(cutoffs)
+
+
+def conduct_mab_ht(baseline_policy, proposed_policy, true_model_list, estimated_model, num_pulls,
+                   t, T, sampling_dbn, alpha, mc_reps=1000):
+  """
+
+  :param baseline_policy:
+  :param proposed_policy:
+  :param true_model_list:
+  :param estimated_model:
+  :param num_pulls:
+  :param t:
+  :param T:
+  :param sampling_dbn:
+  :param mc_reps:
+  :return:
+  """
+  # Get cutoff by searching over possible models
+  sampling_dbns = []
+  for true_model in true_model_list:
+    # ToDo: check if in H0
+    sampling_dbn = mab_regret_sampling_dbn(baseline_policy, proposed_policy, true_model, estimated_model, num_pulls,
+                                           t, T, sampling_dbn, mc_reps=1000)
+    sampling_dbns.append(sampling_dbn)
+  cutoff = cutoff_for_ht(alpha, sampling_dbns)
+
+  # Compare test statistic to cutoff
+  estimated_baseline_regret = true_normal_mab_regret(baseline_policy, estimated_model, estimated_model, num_pulls, t, T,
+                                                     mc_reps=5000)
+  estimated_proposed_regret = true_normal_mab_regret(proposed_policy, estimated_model, estimated_model, num_pulls, t, T,
+                                                     mc_reps=5000)
+  test_statistic = estimated_baseline_regret - estimated_proposed_regret
+  if test_statistic > cutoff:
+    return True
+  else:
+    return False
 
 
 
