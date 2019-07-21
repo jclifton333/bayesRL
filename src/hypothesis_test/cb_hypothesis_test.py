4a42f1d234f8fe95b5cee1690e16bc5a80c6f9da
Jesse Clifton <jclifton333@gmail.com>
2019-07-20 23:46:23 -0400

    debugging cb ht

--- src/hypothesis_test/cb_hypothesis_test.py
+++ src/hypothesis_test/cb_hypothesis_test.py
@@ -13,7 +13,7 @@ sys.path.append(project_dir)
 from src.policies import tuned_bandit_policies as tuned_bandit
 from src.policies import linear_algebra as la
 from functools import partial
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.metrics.pairwise import pairwise_kernels
 import numpy as np
 import copy
@@ -46,6 +46,7 @@ def true_cb_regret(policy, true_model, estimated_model, num_pulls, t, T, pre_gen
     for tprime in range(T-t):
       # Take action
       context_features_tprime = context_features[tprime, rollout]
+      # ToDo: can probably be optimized
       means = [np.dot(beta_hat, context_features_tprime) for beta_hat in estimated_model_rollout[0]]
       a = policy(means, None, num_pulls_rep, tprime)
       reward = reward_draws[a][tprime - t, rollout]
@@ -84,17 +85,17 @@ def cb_sampling_dbn(true_model_params, context_dbn_sampler, feature_function, nu
   sampled_model = []
   p = len(true_model_params[0][0])
   for arm_params, arm_pulls in zip(true_model_params, num_pulls):
-    beta, var_params, _ = true_model_params
+    beta, var_params = arm_params[0], arm_params[1]
     contexts = context_dbn_sampler(arm_pulls)  # Draw contexts
-    context_features = [feature_function(c) for c in contexts]
+    context_features = np.array([feature_function(c) for c in contexts])
     means = np.dot(context_features, beta)  # Get true means and variances at contexts
     variances = np.dot(context_features, var_params)
 
     # Sample beta_hat from sampling_dbn
-    ys = np.random.normal(loc=means, scale=variances)
-    mean_regression = LinearRegression()
-    mean_regression.fit(context_features, ys)  # ToDo: handle intercept
-    beta_hat = mean_regression.coef_
+    ys = np.random.normal(loc=means, scale=np.abs(variances))  # ToDo: fix the negative variance thing!
+    XprimeX = np.dot(context_features.T, context_features)
+    XprimeX_inv = np.linalg.inv(np.diag(p) + XprimeX)
+    beta_hat = np.dot(XprimeX_inv, np.dot(context_features.T, ys))
 
     # Sample theta_hat from sampling_dbn by regressing squared errors on contexts
     # ToDo: allows negative variance, makes no sense!
@@ -103,12 +104,12 @@ def cb_sampling_dbn(true_model_params, context_dbn_sampler, feature_function, nu
     variance_regression.fit(contexts, errors**2)
     var_params_hat = variance_regression.coef_
 
-    sampled_model.append([beta_hat, var_params_hat])
+    sampled_model.append([beta_hat, var_params_hat, XprimeX_inv, context_features, ys])
   return sampled_model
 
 
 def cb_regret_sampling_dbn(baseline_policy, proposed_policy, true_model, estimated_model, num_pulls,
-                            t, T, sampling_dbn, true_mab_regret, pre_generate_mab_data, context_dbn_sampler,
+                           t, T, sampling_dbn, true_mab_regret, pre_generate_mab_data, context_dbn_sampler,
                            feature_function, sampling_dbn_draws=50, reps_to_compute_regret=100):
   """
 
@@ -170,8 +171,8 @@ def pre_generate_cb_data(true_model, context_dbn_sampler, feature_function, T, m
 
 
 def cb_ht_operating_characteristics(baseline_policy, proposed_policy, true_model_list, estimated_model, num_pulls, t, T,
-                                     sampling_dbn_sampler, alpha, true_cb_regret, pre_generate_cb_data,
-                                     true_model_params, context_dbn_sampler, feature_function,
+                                    sampling_dbn_sampler, alpha, true_cb_regret, pre_generate_cb_data,
+                                    true_model_params, context_dbn_sampler, feature_function,
                                      outer_loop_mc_reps=100, inner_loop_mc_reps=100):
   # Get true regrets to see if H0 is true
   draws_from_estimated_model = pre_generate_cb_data(true_model_params, context_dbn_sampler, feature_function, T-t,
@@ -185,8 +186,9 @@ def cb_ht_operating_characteristics(baseline_policy, proposed_policy, true_model
   # Rejection rate
   rejections = []
   for sample in range(outer_loop_mc_reps):
-    estimated_model = sampling_dbn_sampler(true_model_params, num_pulls)
-    reject = conduct_cb_ht(baseline_policy, proposed_policy, true_model_list, estimated_model, num_pulls,
+    estimated_model_ = sampling_dbn_sampler(true_model_params, context_dbn_sampler, feature_function, num_pulls)
+    estimated_model_ += estimated_model[2:]  # Add current estimates
+    reject = conduct_cb_ht(baseline_policy, proposed_policy, true_model_list, estimated_model_, num_pulls,
                            t, T, sampling_dbn_sampler, alpha, true_cb_regret, pre_generate_cb_data,
                            context_dbn_sampler, feature_function, mc_reps=inner_loop_mc_reps)
     rejections.append(reject)
@@ -278,18 +280,19 @@ if __name__ == "__main__":
 
   def context_dbn_sampler(n):
     X = np.random.multivariate_normal(mean=[0.0, 0.0], cov=np.eye(2), size=n)
-    if n == 1:
-      return X[0]
-    else:
-      return X
+    return X
+    # if n == 1:
+    #   return X[0]
+    # else:
+    #   return X
 
   # Do hypothesis test
   true_model_params = [(np.random.normal(size=2), np.random.normal(size=2)),
                        (np.random.normal(size=2), np.random.normal(size=2))]
   Xs = [feature_function(context_dbn_sampler(1)), feature_function(context_dbn_sampler(1))]
-  XprimeX_invs = [la.sherman_woodbury(np.eye(2), x, x) for x in Xs]
-  ys = [np.random.normal(loc=np.dot(p[0], x), scale=np.dot(p[1], x)**2) for x, p in zip(Xs, true_model_params)]
-  beta_hats = [np.dot(xpx_inv, np.dot(x.T, y)) for xpx_inv, x, y in zip(XprimeX_invs, Xs, ys)]
+  XprimeX_invs = [la.sherman_woodbury(np.eye(2), x[0], x[0]) for x in Xs]
+  ys = [np.random.normal(loc=np.dot(p[0], x[0]), scale=np.dot(p[1], x[0])**2) for x, p in zip(Xs, true_model_params)]
+  beta_hats = [np.dot(xpx_inv, np.dot(x.T, y))[:, 0] for xpx_inv, x, y in zip(XprimeX_invs, Xs, ys)]
   theta_hats = [p[1] for p in true_model_params]
   estimated_model = [beta_hats, theta_hats, XprimeX_invs, Xs, ys]
   number_of_pulls = [1, 1]
@@ -303,7 +306,11 @@ if __name__ == "__main__":
                   tuning_function_parameter=tuning_function_parameter, T=T, t=t, env=None)
 
   true_model_list = [[(np.random.normal(loc=p[0]), np.random.normal(loc=p[1])) for p in true_model_params]]
-  for i in range(10):
-    ans = conduct_cb_ht(baseline_policy, proposed_policy, true_model_list, estimated_model, number_of_pulls, t, T,
-                        cb_sampling_dbn, alpha_schedule[t], true_cb_regret,
-                        pre_generate_cb_data, context_dbn_sampler, feature_function, mc_reps=100)
+  for i in range(1):
+    operating_char_dict = cb_ht_operating_characteristics(baseline_policy, proposed_policy, true_model_list,
+                                                          estimated_model, number_of_pulls,
+                                                          t, T, cb_sampling_dbn, alpha_schedule[t],
+                                                          true_cb_regret,
+                                                          pre_generate_cb_data, true_model_params, context_dbn_sampler,
+                                                          feature_function,
+                                                          inner_loop_mc_reps=10, outer_loop_mc_reps=10)
