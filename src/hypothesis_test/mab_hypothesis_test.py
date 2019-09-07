fix null prob calculation

src/hypothesis_test/mab_hypothesis_test.py
@@ -38,11 +38,12 @@ def approximate_posterior_h0_prob(empirical_dbn, epsilon=0.2, df=3):
   if len(np.where(bins_ <= 0)[0]) > 0:
     total_mass = np.sum(posterior_density)
     mass_less_than_0 = np.sum(posterior_density[np.where(bins_ <= 0)])
-    odds_ratio = mass_less_than_0 / total_mass
+    probability_less_than_0 = mass_less_than_0 / total_mass
+    odds_ratio = probability_less_than_0 / (1 - probability_less_than_0)
 
     # Get correction factor
-    best_null_value = np.max(posterior_density[np.where(bins_ <= 0)])
-    correction = 1 + (epsilon * best_null_value) / ((1-epsilon)*(1-mass_less_than_0)*total_mass)
+    best_null_value = np.max(posterior_density[np.where(bins_ <= 0)]) / total_mass
+    correction = 1 + (epsilon * best_null_value) / ((1-epsilon)*(1-probability_less_than_0)*total_mass)
     corrected_odds_ratio = odds_ratio * correction
 
     # Probability