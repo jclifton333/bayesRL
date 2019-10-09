65e013827c17141c87d14b9c2f7db7f1f2c27560
Jesse Clifton <jclifton333@gmail.com>
2019-10-09 00:26:32 -0400

    fix propensities

--- src/run/normal_mab_ipw_approx.py
+++ src/run/normal_mab_ipw_approx.py
@@ -7,15 +7,18 @@ sys.path.append(project_dir)
 
 
 import src.policies.ipw_regret_estimate as ipw
+import src.hypothesis_test.mab_hypothesis_test as ht
 from src.environments.Bandit import NormalMAB
 import numpy as np
 from functools import partial
 import datetime
 import yaml
 import multiprocessing as mp
+from scipy.stats import norm
 
 
 def episode(label, tune=True, std=0.1, list_of_reward_mus=[0.0,0.1], T=50, out_of_sample_size=100):
+  np.random.seed(label)
   env = NormalMAB(list_of_reward_mus=list_of_reward_mus, list_of_reward_vars=[std**2]*len(list_of_reward_mus))
   lower_bound = 0.1
   upper_bound = 0.55
@@ -23,9 +26,18 @@ def episode(label, tune=True, std=0.1, list_of_reward_mus=[0.0,0.1], T=50, out_o
   cumulative_regret = 0.0
   mu_opt = np.max(env.list_of_reward_mus)
   env.reset()
+
+  # Initialize lists that will be needed to compute IPWEs
+  actions = []
+  action_probs = []
+  rewards = []
+
   # Initial pulls
   for a in range(env.number_of_actions):
-    env.step(a)
+    res_ = env.step(a)
+    rewards.append(res_['Utility'])
+    actions.append(a)
+    action_probs.append(0.5)
 
   # Initial propensities
   pi_tilde = 0.5
@@ -45,11 +57,14 @@ def episode(label, tune=True, std=0.1, list_of_reward_mus=[0.0,0.1], T=50, out_o
 
     if tune:
       # Get best epsilon using ipw estimator
+      mu_ipw, std_ipw = ht.ipw(env.number_of_actions, actions, action_probs, rewards)
+      se_ipw = std_ipw / np.sqrt(t + 1)  # Pooled estimate
+
       # Get confidence intervals to form range for minimax
-      mu_1_upper_conf = env.estimated_means[0] + 1.96*env.standard_errors[0]
-      mu_1_lower_conf = env.estimated_means[0] - 1.96*env.standard_errors[0]
-      mu_2_upper_conf = env.estimated_means[1] + 1.96*env.standard_errors[1]
-      mu_2_lower_conf = env.estimated_means[1] - 1.96*env.standard_errors[1]
+      mu_1_upper_conf = mu_ipw[0] + 1.96*se_ipw
+      mu_1_lower_conf = mu_ipw[0] - 1.96*se_ipw
+      mu_2_upper_conf = mu_ipw[1] + 1.96*se_ipw
+      mu_2_lower_conf = mu_ipw[1] - 1.96*se_ipw
 
       min_range_ = np.max((lower_bound, mu_2_lower_conf - mu_1_upper_conf))
       max_range_ = np.min((upper_bound, mu_2_upper_conf - mu_1_lower_conf))
@@ -57,8 +72,9 @@ def episode(label, tune=True, std=0.1, list_of_reward_mus=[0.0,0.1], T=50, out_o
                                          (pi_inv_sum, m_pi_inv_sum), t)
       epsilon_sequence.append(float(best_epsilon))
 
-      pi_inv_sum += 1 / arm0_prob  # ToDo: Check that this is the correct arm
-      m_pi_inv_sum += 1 / (1 - arm0_prob)
+      propensity = 1 - norm.cdf(-(mu_ipw[0] - mu_ipw[1]) / np.sqrt(2*se_ipw))
+      pi_inv_sum += 1 / propensity
+      m_pi_inv_sum += 1 / (1 - propensity)
 
     # Get eps-greedy action
     if np.random.uniform() < arm0_prob:
@@ -102,6 +118,7 @@ def run(replicates, tune=True, save=True):
     filename = '{}_{}.yml'.format(prefix, suffix)
     with open(filename, 'w') as outfile:
       yaml.dump(results, outfile)
+
 
 if __name__ == "__main__":
   np.random.seed(3)
